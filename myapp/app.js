a05205c8155d312b66ccdbd8f8eb03673fa64741
HPathakCS <pathakhimanshucs@gmail.com>
2018-12-06 00:16:09 +0100

    LITERALLY WHOLE FORKING GAME

--- myapp/app.js
+++ myapp/app.js
@@ -13,10 +13,25 @@ const socketServer = new WebSocket.Server({ server });
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-app.get("/splash", indexRouter);
 app.get("/play", indexRouter);
+app.get("/*", indexRouter);
 
+var games = [];
+var board = [];
+initializeBoard();
 
+// var game = {
+//     numPlayers: value,
+//     playerA: value, //Websocket of player
+//     playerB: value,
+//     playerC: value,
+//     playerD: value,
+//     playerAPos: value, //Position of players
+//     playerBPos: value,
+//     playerCPos: value,
+//     playerDPos: value,
+//     gameState: //"Completed", "Waiting", "Ongoing"
+// }
 
 socketServer.on("connection", function(ws){
     console.log("Connection made");
@@ -24,11 +39,145 @@ socketServer.on("connection", function(ws){
         var msg = JSON.parse(message);
         switch(msg.type){
             case "rollDice":
-                var player = msg.player;
-                ws.send(JSON.stringify({
-                    type: "roll",
-                    roll: rollRandomDice()
-                }));
+            var game = games[msg.gameId];
+            var diceResult = rollRandomDice();
+            ws.send(JSON.stringify({
+                type: "roll",
+                roll: diceResult
+            }));
+            var currPos;
+            switch(msg.player){
+                    case "A":
+                        currPos = game.playerAPos;
+                        game.playerAPos = validateMove(currPos + diceResult);
+                        break;
+                    case "B":
+                        currPos = game.playerBPos;
+                        game.playerBPos = validateMove(currPos + diceResult);
+                        break;
+                    case "C":
+                        currPos = game.playerCPos;
+                        game.playerCPos = validateMove(currPos + diceResult);
+                        break;
+                    case "D":
+                        currPos = game.playerDPos;
+                        game.playerDPos = validateMove(currPos + diceResult);
+                        break;
+                }
+                switch(game.numPlayers){
+                    case 2:
+                        var gameUpdate = JSON.stringify({
+                            type: "gameUpdate",
+                            playerAPos: game.playerAPos,
+                            playerBPos: game.playerBPos 
+                         });
+                        game.playerA.send(gameUpdate);
+                        game.playerB.send(gameUpdate);
+                        break;
+                    case 3:
+                        var gameUpdate = JSON.stringify({
+                            type: "gameUpdate",
+                            playerAPos: game.playerAPos,
+                            playerBPos: game.playerBPos,
+                            playerCPos: game.playerCPos
+                        });
+                        game.playerA.send(gameUpdate);
+                        game.playerB.send(gameUpdate);
+                        game.playerC.send(gameUpdate);
+                        break;
+                    case 4:
+                        var gameUpdate = JSON.stringify({
+                            type: "gameUpdate",
+                            playerAPos: game.playerAPos,
+                            playerBPos: game.playerBPos,
+                            playerCPos: game.playerCPos,
+                            playerDPos: game.playerDPos
+                        });
+                        game.playerA.send(gameUpdate);
+                        game.playerB.send(gameUpdate);
+                        game.playerC.send(gameUpdate);
+                        game.playerD.send(gameUpdate);
+                        break;    
+
+                }
+                break;
+            case "initialize":
+                var found = false;
+                for (let index = 0; index < games.length; index++) {
+                    const element = games[index];  
+                    if(element.gameState == "Waiting" && element.numPlayers == msg.numPlayers){
+                        if(element.playerA == -1){
+                            element.playerA = ws;
+                            ws.send(JSON.stringify({
+                                type: "gameJoin",
+                                gameId: index,
+                                playerTag: "A"
+                            }));
+                            found = true;
+                        }else if(element.playerB == -1){
+                            element.playerB = ws;
+                            ws.send(JSON.stringify({
+                                type: "gameJoin",
+                                gameId: index,
+                                playerTag: "B"
+                            }));
+                            if(element.numPlayers == 2){
+                                element.playerC = -2;
+                                element.playerD = -2;
+                                element.gameState = "Ongoing";
+                            }
+                            found = true;
+                        }else if(element.playerC == -1){
+                            element.playerC = ws;
+                            ws.send(JSON.stringify({
+                                type: "gameJoin",
+                                gameId: index,
+                                playerTag: "C"
+                            }));
+                            if(element.numPlayers == 3){
+                                element.playerD = -2;
+                                element.gameState = "Ongoing";
+                            }
+                            found = true;
+                        }else if(element.playerD == -1){
+                            element.playerD = ws;
+                            ws.send(JSON.stringify({
+                                type: "gameJoin",
+                                gameId: index,
+                                playerTag: "D"
+                            }));
+                            if(element.numPlayers == 4){
+                                element.gameState = "Ongoing";
+                            }
+                            found = true;
+                        }
+                    }
+                };
+                if(!found){
+                    games.push({
+                        numPlayers: msg.numPlayers,
+                        playerA: ws, //Websocket of player
+                        playerB: -1,
+                        playerC: -1,
+                        playerD: -1,
+                        playerAPos: 1, //Position of players
+                        playerBPos: 1,
+                        playerCPos: 1,
+                        playerDPos: 1,
+                        gameState: "Waiting"
+                    });
+
+                    ws.send(JSON.stringify({
+                        type: "gameJoin",
+                        gameId: games.length -1,
+                        playerTag: "A"
+                    }));
+
+                    console.log("Created new game object");
+                    console.log(games);
+                }
+                break;
+                
         }
         console.log("[LOG] " + message);
     });
@@ -36,6 +185,25 @@ socketServer.on("connection", function(ws){
 
 function rollRandomDice(){
     return Math.floor(Math.random() * 6) + 1;
+}
+
+function initializeBoard(){
+    for (let index = 0; index < 100; index++) {
+        board[index] = -1;
+    }
+
+    //snakes
+    board[99]=21; board[95]=75; board[93]=89; board[78]=25; board[52]=28; board[16]=8;
+    //ladders
+    board[2]=45; board[4]=27; board[9]=31; board[47]=84; board[70]=87; board[71]=91;
+}
+
+function validateMove(position){
+    if(board[position] == -1){
+        return position;
+    }else{
+        return board[position];
+    }
 }
 
 server.listen(port);
--- myapp/public/javascripts/client.js
+++ myapp/public/javascripts/client.js
@@ -2,6 +2,7 @@ var canvas = document.getElementById("gameCanvas");
 var context = canvas.getContext("2d");
 
 
+
 function drawOnCanvas(player, position){
     var y=Math.floor(position/10);
     var x=Math.floor(position%10)-1;
@@ -44,33 +45,79 @@ function drawOnCanvas(player, position){
         context.closePath();
 }
 
-drawOnCanvas(1,99);
-drawOnCanvas(2,25);
-drawOnCanvas(3,32);
-drawOnCanvas(4,49);
-
-
-var socket = new WebSocket("ws://localhost:3000");
-function rollDice(){
-    console.log("button click");
-        socket.send(JSON.stringify({
-            type: "rollDice",
-            player: "A"
-        }));
-};
-
-socket.onopen = function(){
-    console.log("connected");
+function clearCanvas(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+var socket;
+var playerTag;
+var gameId;
+function startGame(numPlayers){
+    document.getElementById("playerButtons").remove();
+    console.log("Starting a " + numPlayers + " player game");
+    
+    drawOnCanvas(1,0);
+    drawOnCanvas(2,0);
+    drawOnCanvas(3,0);
+    drawOnCanvas(4,0);
 
-socket.onmessage = function(event){
-    var json = JSON.parse(event.data);
-    switch(json.type){
-        case "roll":
-            console.log("Server rolled " + json.roll);
+    socket = new WebSocket("ws://localhost:3000");
+    socket.onopen = function(){
+        console.log("connected");
+        socket.send(JSON.stringify({
+                type: "initialize",
+                numPlayers: numPlayers
+        }));
+    }
+
+
+    socket.onmessage = function(event){
+        var json = JSON.parse(event.data);
+        switch(json.type){
+            case "roll":
+                console.log("Server rolled " + json.roll);
+                break;
+            case "gameJoin":
+                console.log("We are player " + json.playerTag + " in gameId " + json.gameId);
+                playerTag = json.playerTag;
+                gameId = json.gameId;
+                break;
+            case "gameUpdate":
+                switch(numPlayers){
+                    case 2:
+                        clearCanvas();
+                        drawOnCanvas(1, json.playerAPos);
+                        drawOnCanvas(2, json.playerBPos);
+                        break;
+                    case 3:
+                        clearCanvas();
+                        drawOnCanvas(1, json.playerAPos);
+                        drawOnCanvas(2, json.playerBPos);
+                        drawOnCanvas(3, json.playerCPos);
+                        break;
+                    case 4:
+                        clearCanvas();
+                        drawOnCanvas(1, json.playerAPos);
+                        drawOnCanvas(2, json.playerBPos);
+                        drawOnCanvas(3, json.playerCPos);
+                        drawOnCanvas(4, json.playerDPos);
+                        break;
+
+                }
+
+        }
     }
 }
-    
+
+function rollDice(){
+    console.log("button click");
+    socket.send(JSON.stringify({
+        type: "rollDice",
+        gameId: gameId,
+        player: playerTag
+    }));
+};
+
+
 
 console.log("client.js reporting");
