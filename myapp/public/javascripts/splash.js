33eb4df74444b7d1cde6a21750501b8dc491bfe0
HPathakCS <pathakhimanshucs@gmail.com>
2018-12-11 11:04:13 +0100

    kek

--- myapp/public/javascripts/splash.js
+++ myapp/public/javascripts/splash.js
@@ -6,4 +6,3 @@ if(!document.cookie == 0){
 
 var cookieText = document.getElementById("cookie");
 cookieText.innerText = "You visited this page " + document.cookie + " times";
-console.log(document.cookie);
